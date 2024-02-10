6a61dd661188b9107afd5a8d3f13a868c4f23e32
Mingle Li <carson.whitehouse@gmail.com>
2024-02-10 17:31:26 -0500

    Switched intake motors talon to sparkMax

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -8,20 +10,20 @@ import frc.robot.constants.Constants;
 import frc.robot.constants.IntakeConstants;
 
 public class IntakeSubsystem extends SubsystemBase {
-    private final TalonFX m_talon_pull = new TalonFX(Constants.INTAKE_MOTOR_ID);
+    private final CANSparkMax m_sparkMax_pull = new CANSparkMax(Constants.INTAKE_MOTOR_ID, MotorType.kBrushless);
 
     public IntakeSubsystem() {
-        m_talon_pull.setInverted(true);
+        m_sparkMax_pull.setInverted(true);
     }
 
     public void pull() {
         double pullSpeed = SmartDashboard.getNumber("intakePullVoltage", IntakeConstants.INTAKE_PULL_VOLTS);
 
-        m_talon_pull.setVoltage(pullSpeed);
+        m_sparkMax_pull.setVoltage(pullSpeed);
     }
 
     public void stop() {
-        m_talon_pull.setVoltage(0);
+        m_sparkMax_pull.setVoltage(0);
     }
 
 }
