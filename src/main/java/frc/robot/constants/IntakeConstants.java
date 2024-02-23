7a4103ab8af8f7b0e293e058a2e6b5f2bf8a2e8c
btho7793 <77469772+btho7793@users.noreply.github.com>
2024-02-22 19:06:50 -0500

    do that for real

--- src/main/java/frc/robot/constants/IntakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -4,6 +4,9 @@ public class IntakeConstants {
     public static final double INTAKE_PULL_RPM = 1500;
     public static final double INTAKE_PUSH_RPM = 1500;
 
+    public static final double INTAKE_PULL_VOLTS = 4;
+    public static final double INTAKE_PUSH_VOLTS = 4;
+
     public static final double INTAKE_KP = 0.0002;
     public static final double INTAKE_KI = 0;
     public static final double INTAKE_KD = 0;
--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -2,75 +2,32 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.RelativeEncoder;
 
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.Constants;
 import frc.robot.constants.IntakeConstants;
 
 public class IntakeSubsystem extends SubsystemBase {
-    private PIDController m_pid;
-    private SimpleMotorFeedforward m_feedForward;
-
     private final CANSparkMax m_sparkMax_pull = new CANSparkMax(Constants.INTAKE_MOTOR_ID, MotorType.kBrushless);
-
-    private final RelativeEncoder m_encoder_pull = m_sparkMax_pull.getEncoder();
 
     public IntakeSubsystem() {
         m_sparkMax_pull.setInverted(true);
-
-        m_pid = new PIDController(0, 0, 0);
-
-        m_feedForward = new SimpleMotorFeedforward(0, 0);
-
-        updateProportions();
-    }
-
-    private void updateProportions(){
-        double kp = SmartDashboard.getNumber("intakeKP", IntakeConstants.INTAKE_KP);
-        double ki = SmartDashboard.getNumber("intakeKI", IntakeConstants.INTAKE_KI);
-        double kd = SmartDashboard.getNumber("intakeKD", IntakeConstants.INTAKE_KD);
-
-        double ks = SmartDashboard.getNumber("intakeKS", IntakeConstants.INTAKE_KS);
-        double kv = SmartDashboard.getNumber("intakeKV", IntakeConstants.INTAKE_KV);
-
-        m_pid.setPID(kp, ki, kd);
-        m_feedForward = new SimpleMotorFeedforward(ks, kv);
-    }
-
-    public void periodic(){
-        SmartDashboard.putNumber("intakeOutput", getMeasurement());
-    }
-
-    public void useOutput(){
-        if(m_pid.getSetpoint() != 0){
-            m_sparkMax_pull.setVoltage(m_pid.calculate(getMeasurement()) + m_feedForward.calculate(m_pid.getSetpoint()));
-        } else {
-            m_sparkMax_pull.setVoltage(0);
-        }
-    }
-
-    public double getMeasurement(){
-        return m_encoder_pull.getVelocity();
     }
 
     public void pull() {
-        double pullSpeed = SmartDashboard.getNumber("intakePullRPM", IntakeConstants.INTAKE_PULL_RPM);
+        double pullSpeed = SmartDashboard.getNumber("intakePullVoltage", IntakeConstants.INTAKE_PULL_VOLTS);
 
-        m_pid.setSetpoint(pullSpeed);
+        m_sparkMax_pull.setVoltage(pullSpeed);
     }
 
     public void push() {
-        double pushSpeed = SmartDashboard.getNumber("intakePushRPM", IntakeConstants.INTAKE_PUSH_RPM);
+        double pushSpeed = SmartDashboard.getNumber("intakePushVoltage", IntakeConstants.INTAKE_PUSH_VOLTS);
 
-        m_pid.setSetpoint(pushSpeed);
+        m_sparkMax_pull.setVoltage(pushSpeed);
     }
 
     public void stop() {
-        m_pid.setSetpoint(0);
+        m_sparkMax_pull.setVoltage(0);
     }
-
 }
