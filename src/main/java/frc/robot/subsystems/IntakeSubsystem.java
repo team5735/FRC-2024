1a1642e01fdf7bd4ab5d7d8f77558faaf8cbef5d
btho7793 <77469772+btho7793@users.noreply.github.com>
2024-02-22 19:04:53 -0500

    switch to non-pid version of intakesubsystem

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -42,8 +42,6 @@ public class IntakeSubsystem extends SubsystemBase {
     }
 
     public void periodic(){
-        // updateProportions();
-        
         SmartDashboard.putNumber("intakeOutput", getMeasurement());
     }
 
