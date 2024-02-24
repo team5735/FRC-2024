d9a10ea900e1613088b86b14df3278203bb1c510
Mingle Li <carson.whitehouse@gmail.com>
2024-02-23 19:57:27 -0500

    Removed one redundant call

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -106,7 +106,6 @@ public class Robot extends TimedRobot {
         // block in order for anything in the Command-based framework to work.
         CommandScheduler.getInstance().run();
 
-        RobotContainer.m_feederSubsystem.periodic();
     }
 
     /**
