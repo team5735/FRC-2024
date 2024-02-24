c40b5f54269de531d2bb2098b48b3c5b881ecc7c
Mingle Li <carson.whitehouse@gmail.com>
2024-02-24 16:07:17 -0500

    Forgot to fix this in the last commit

--- src/main/java/frc/robot/commands/intake/IntakeCommandOut.java
+++ src/main/java/frc/robot/commands/intake/IntakeCommandOut.java
@@ -14,6 +14,7 @@ public class IntakeCommandOut extends Command {
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
+        m_subsystem.push();
     }
 
     // Called every time the scheduler runs while the command is scheduled.
