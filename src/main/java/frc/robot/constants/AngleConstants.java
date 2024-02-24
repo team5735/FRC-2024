6500ce2dad073b023f46ed8c409bda0214a61ace
Mingle Li <carson.whitehouse@gmail.com>
2024-02-24 16:13:53 -0500

    Integrating the angle changer (WIP) from the PID-&-Feedforward branch. All code is now on the same branch of the same repo!!!

--- src/main/java/frc/robot/constants/AngleConstants.java
+++ src/main/java/frc/robot/constants/AngleConstants.java
@@ -1,15 +1,28 @@
 package frc.robot.constants;
 
 public class AngleConstants {
-    public static final double ANGLE_KP = 0;
-    public static final double ANGLE_KI = 0;
-    public static final double ANGLE_KD = 0;
+    public static final double ANGLE_KP = 0.052;
+    public static final double ANGLE_KI = 0.04;
+    public static final double ANGLE_KD = 0.018;
 
     public static final double ANGLE_KS = 0;
-    public static final double ANGLE_KG = 0;
+    public static final double ANGLE_KG = 0.515;
     public static final double ANGLE_KV = 0;
 
-    public static double convertTalonRotationsToDegrees(double rotations) {
+
+    public static final double ANGLE_START_POS_ROT = 335./360; // This means 335 degrees (unit of device rotations)
+    public static final double ANGLE_START_POS_DEG = convertRotationsToDegrees(ANGLE_START_POS_ROT);
+
+    public static final double ANGLE_HIGHEST_DEG = 338;
+    public static final double ANGLE_LOWEST_DEG = 230;
+
+    public static final double ANGLE_MOTOR_TO_OUTPUT_RATIO = 1./9 * 22./42; 
+    // planetary 9:1, pulleys 22:42 (they are divided by one another, leading to this inverse ratio above)
+    public static final double ANGLE_OUTPUT_TO_MOTOR_RATIO = 1./ANGLE_MOTOR_TO_OUTPUT_RATIO;
+    // inverse of the previous ratio
+
+
+    public static double convertRotationsToDegrees(double rotations) {
         rotations %= 1;
         if (rotations < 0)
             rotations += 1;
