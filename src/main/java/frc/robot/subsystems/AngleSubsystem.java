5ac110db62cdde9ac1489ab9a799901bb2f87246
Mingle Li <carson.whitehouse@gmail.com>
2024-02-27 20:21:44 -0500

    fuck microsoft (kindly)

--- src/main/java/frc/robot/subsystems/AngleSubsystem.java
+++ src/main/java/frc/robot/subsystems/AngleSubsystem.java
@@ -1,15 +1,12 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.AbsoluteEncoder;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
 
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.AnalogEncoder;
-import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.AngleConstants;
