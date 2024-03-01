79c0eedd52225aad1f70112d78b19710ce986a05
btho7793 <77469772+btho7793@users.noreply.github.com>
2024-02-29 19:41:12 -0500

    Make pathconstants and use it in turn90command

--- src/main/java/frc/robot/commands/drivetrain/Turn90Command.java
+++ src/main/java/frc/robot/commands/drivetrain/Turn90Command.java
@@ -4,6 +4,7 @@ import java.util.function.Supplier;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.constants.PathConstants;
 import frc.robot.subsystems.DrivetrainSubsystem;
 
 public class Turn90Command extends Command {
@@ -17,23 +18,17 @@ public class Turn90Command extends Command {
         m_drivetrain = drivetrain;
 
         m_controller.enableContinuousInput(-Math.PI, Math.PI);
-        m_controller.setTolerance(0.001);
+        m_controller.setTolerance(PathConstants.TURN_90_TOLERANCE);
 
         addRequirements(m_drivetrain);
     }
-
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
     @Override
     public void initialize() {
         m_startPos = getCurrentAngle.get() % (Math.PI * 2);
     }
 
     public void execute() {
-        m_drivetrain.drive(0, 0,
-                m_controller.calculate(getCurrentAngle.get(), m_startPos + m_distance));
-    }
-
-    @Override
-    public boolean isFinished() {
-        return m_controller.atSetpoint();
+        m_drivetrain.drive(m_controller.calculate(getCurrentAngle.get(), m_startPos + m_distance));
     }
 }
---
+++ src/main/java/frc/robot/constants/PathConstants.java
@@ -0,0 +1,5 @@
+package frc.robot.constants;
+
+public class PathConstants {
+    public static final double TURN_90_TOLERANCE = 0.001;
+}
