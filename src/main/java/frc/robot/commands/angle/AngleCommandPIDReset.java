2e7956b449e6d6b17a01134b9fe4e0ff4d1713e3
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2024-03-10 15:18:34 -0400

    remove empty line

--- src/main/java/frc/robot/commands/angle/AngleCommandPIDReset.java
+++ src/main/java/frc/robot/commands/angle/AngleCommandPIDReset.java
@@ -31,5 +31,4 @@ public class AngleCommandPIDReset extends Command {
     public boolean isFinished() {
         return true;
     }
-
 }
--- src/main/java/frc/robot/commands/angle/AngleCommandReleaseMotors.java
+++ src/main/java/frc/robot/commands/angle/AngleCommandReleaseMotors.java
@@ -3,7 +3,7 @@ package frc.robot.commands.angle;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.AngleSubsystem;
 
-public class AngleCommandReleaseMotors extends Command{
+public class AngleCommandReleaseMotors extends Command {
     AngleSubsystem m_subsystem;
 
     public AngleCommandReleaseMotors(AngleSubsystem s) {
@@ -20,7 +20,6 @@ public class AngleCommandReleaseMotors extends Command{
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        // System.out.println("hellotest");
     }
 
     // Called once the command ends or is interrupted.
@@ -34,5 +33,4 @@ public class AngleCommandReleaseMotors extends Command{
     public boolean isFinished() {
         return false;
     }
-    
 }
