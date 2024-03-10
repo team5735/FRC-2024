4ae51a7907ff98f42183ae726946c09719f6c2c1
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2024-03-10 15:28:47 -0400

    remove old constant, move comments to above their line

--- src/main/java/frc/robot/constants/AngleConstants.java
+++ src/main/java/frc/robot/constants/AngleConstants.java
@@ -2,7 +2,6 @@ package frc.robot.constants;
 
 public class AngleConstants {
     public static final double ANGLE_KP = 0.0625;
-    // public static final double ANGLE_KP = 0;
     public static final double ANGLE_KI = 0;
     public static final double ANGLE_KD = 0;
 
@@ -10,8 +9,7 @@ public class AngleConstants {
     public static final double ANGLE_KG = 0.78;
     public static final double ANGLE_KV = 0;
 
-
-    public static final double ANGLE_START_POS_ROT = 235./360; // This means 335 degrees (unit of device rotations)
+    public static final double ANGLE_START_POS_ROT = 235. / 360; // This means 335 degrees (unit of device rotations)
     public static final double ANGLE_START_POS_DEG = convertRotationsToDegrees(ANGLE_START_POS_ROT);
 
     public static final double ANGLE_HIGHEST_DEG = 237;
@@ -20,11 +18,10 @@ public class AngleConstants {
     public static final double ANGLE_AMP_DEG = 125;
     public static final double ANGLE_FARTHEST_SHOOT_DEG = 190;
 
-    public static final double ANGLE_MOTOR_TO_OUTPUT_RATIO = 1./9 * 16./42; 
-    // planetary 9:1, chain drive 16:42 
-    public static final double ANGLE_OUTPUT_TO_MOTOR_RATIO = 1./ANGLE_MOTOR_TO_OUTPUT_RATIO;
+    // planetary 9:1, chain drive 16:42
+    public static final double ANGLE_MOTOR_TO_OUTPUT_RATIO = 1. / 9 * 16. / 42;
     // inverse of the previous ratio
-
+    public static final double ANGLE_OUTPUT_TO_MOTOR_RATIO = 1. / ANGLE_MOTOR_TO_OUTPUT_RATIO;
 
     public static double convertRotationsToDegrees(double rotations) {
         rotations %= 1;
