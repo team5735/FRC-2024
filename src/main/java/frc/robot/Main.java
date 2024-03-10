d7730e45b4b1cb2b95ebd80daaedca0d60bf1c52
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2024-03-10 15:37:33 -0400

    format main.java

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -12,12 +12,14 @@ import edu.wpi.first.wpilibj.RobotBase;
  * the parameter class to the startRobot call.
  */
 public final class Main {
-    private Main() {}
+    private Main() {
+    }
 
     /**
      * Main initialization function. Do not perform any initialization here.
      *
-     * <p>If you change your main robot class, change the parameter type.
+     * <p>
+     * If you change your main robot class, change the parameter type.
      */
     public static void main(String... args) {
         RobotBase.startRobot(Robot::new);
