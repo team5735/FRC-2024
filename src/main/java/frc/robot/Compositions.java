47a121254f96a491c3c726f824f99847827cd336
btho7793 <77469772+btho7793@users.noreply.github.com>
2024-03-16 17:31:10 -0400

    fix pid shoot by using a cool keyword,  more normal auto, etc

--- src/main/java/frc/robot/Compositions.java
+++ src/main/java/frc/robot/Compositions.java
@@ -19,29 +19,29 @@ import frc.robot.subsystems.shooter.ShooterTopSubsystem;
  * belong to
  */
 public class Compositions {
-    /*
-     * Creates and returns a new SequentialCommandGroup that first spins up the
-     * shooter, that is to say it gets the shooter to full speed, and then has a
-     * ParallelCommandGroup that feeds the NOTE in, and simultaneously keeps the
-     * shooter at full speed.
-     */
-    static Command feedAndShoot(FeederSubsystem feeder, ShooterTopSubsystem shooterTop,
-            ShooterBottomSubsystem shooterBottom) {
-        return new SequentialCommandGroup(
-                new ShooterSpinUpCommand(shooterTop, shooterBottom),
-                new ParallelCommandGroup(
-                        new FeederCommandIn(feeder),
-                        new ShooterHoldNStopCommand(shooterTop, shooterBottom)));
-    }
+        /*
+         * Creates and returns a new SequentialCommandGroup that first spins up the
+         * shooter, that is to say it gets the shooter to full speed, and then has a
+         * ParallelCommandGroup that feeds the NOTE in, and simultaneously keeps the
+         * shooter at full speed.
+         */
+        static Command feedAndShoot(FeederSubsystem feeder, ShooterTopSubsystem shooterTop,
+                        ShooterBottomSubsystem shooterBottom) {
+                return new SequentialCommandGroup(
+                                new ShooterSpinUpCommand(shooterTop, shooterBottom),
+                                new ParallelCommandGroup(
+                                                new FeederCommandIn(feeder),
+                                                new ShooterHoldNStopCommand(shooterTop, shooterBottom)));
+        }
 
-    public static Command angleUpdateWithIntake(Command angleSetCommand, AngleSubsystem angler,
-            IntakeSubsystem intake) {
-        return (angler.isAtBase())
-                ? new ParallelCommandGroup(
-                        angleSetCommand,
-                        new ParallelDeadlineGroup(
-                                new WaitCommand(2),
-                                intake.pullCommand()))
-                : angleSetCommand;
-    }
+        public static Command angleUpdateWithIntake(Command angleSetCommand, AngleSubsystem angler,
+                        IntakeSubsystem intake) {
+                return (angler.isAtBase())
+                                ? new ParallelCommandGroup(
+                                                angleSetCommand,
+                                                new ParallelDeadlineGroup(
+                                                                new WaitCommand(2),
+                                                                intake.getPull()))
+                                : angleSetCommand;
+        }
 }
