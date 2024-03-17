f0c7386427f19d60a9471d1fa18fcb3e440d74b9
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2024-03-17 13:50:56 -0400

    document autocommands

--- src/main/java/frc/robot/AutoCommands.java
+++ src/main/java/frc/robot/AutoCommands.java
@@ -12,6 +12,27 @@ import frc.robot.subsystems.shooter.ShooterBottomSubsystem;
 import frc.robot.subsystems.shooter.ShooterTopSubsystem;
 
 public class AutoCommands {
+    /**
+     * Registers named commands. This makes four commands listed below and registers
+     * them using {@link NamedCommands.registerCommands}.
+     *
+     * <p>
+     * <ul>
+     * <li>intake.pullCommand() → startIntake
+     * <li>intake.stopCommand() → stopIntake
+     * <li>Compositions.feedAndShoot(feeder, shooterTop, shooterBottom) →
+     * startShooting
+     * <li>shooterTop.stopCommand() → stopShooting
+     * </ul>
+     *
+     * @param intake        The intake subsystem. Required by startIntake,
+     *                      stopIntake
+     * @param feeder        The feeder subsystem. Required by startShooting
+     * @param shooterTop    The top shooter subsystem. Required by startShooting,
+     *                      stopShooting
+     * @param shooterBottom The bottom shooter subsystem. Required by startShooting,
+     *                      stopShooting
+     */
     static void registerCommands(final IntakeSubsystem intake, final FeederSubsystem feeder,
             final ShooterTopSubsystem shooterTop, final ShooterBottomSubsystem shooterBottom) {
         Map<String, Command> commandsToRegister = new HashMap<>();
