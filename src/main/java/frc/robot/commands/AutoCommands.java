2dddab81c64b5a5fa2d76f2109e442c8f54f2a24
btho7793 <77469772+btho7793@users.noreply.github.com>
2024-03-20 10:06:36 -0400

    more auto

--- src/main/java/frc/robot/commands/AutoCommands.java
+++ src/main/java/frc/robot/commands/AutoCommands.java
@@ -10,8 +10,10 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Compositions;
 import frc.robot.commands.feeder.FeederPrimeNote;
+import frc.robot.commands.feeder.FeederUnprimeNote;
 import frc.robot.constants.ShooterConstants;
 import frc.robot.subsystems.FeederSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
@@ -49,18 +51,16 @@ public class AutoCommands {
     }
 
     public static Command getNote(IntakeSubsystem intake, FeederSubsystem feeder) {
-        return new ParallelDeadlineGroup(new FeederPrimeNote(feeder), intake.getPullStop());
+        return new SequentialCommandGroup(new ParallelDeadlineGroup(new FeederPrimeNote(feeder), intake.getPullStop()),
+                new FeederUnprimeNote(feeder));
     }
 
     public static Command shooterStart(ShooterTopSubsystem top, ShooterBottomSubsystem bottom) {
         return new ParallelCommandGroup(
-            Commands.runOnce(() -> top.setSetpoint(
-                SmartDashboard.getNumber("shootTopRPM", ShooterConstants.SHOOTER_TOP_DEFAULT_RPM)
-            )), 
-            Commands.runOnce(() -> bottom.setSetpoint(
-                SmartDashboard.getNumber("shootBottomRPM", ShooterConstants.SHOOTER_BOTTOM_DEFAULT_RPM)
-            ))
-        );
+                Commands.runOnce(() -> top.setSetpoint(
+                        SmartDashboard.getNumber("shootTopRPM", ShooterConstants.SHOOTER_TOP_DEFAULT_RPM))),
+                Commands.runOnce(() -> bottom.setSetpoint(
+                        SmartDashboard.getNumber("shootBottomRPM", ShooterConstants.SHOOTER_BOTTOM_DEFAULT_RPM))));
     }
 
     public static Command stopShooter(ShooterTopSubsystem top, ShooterBottomSubsystem bottom) {
