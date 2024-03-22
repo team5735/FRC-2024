25a89099eee58ff615ea014cad0adaaca1209687
btho7793 <77469772+btho7793@users.noreply.github.com>
2024-03-21 20:45:08 -0400

    add call to resetShooterShenanigans in Robot.java:teleopInit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -165,6 +165,7 @@ public class Robot extends TimedRobot {
         if (m_autonomousCommand != null) {
             m_autonomousCommand.cancel();
         }
+        m_robotContainer.resetShooterShenanigans();
     }
 
     /** This function is called periodically during operator control. */
