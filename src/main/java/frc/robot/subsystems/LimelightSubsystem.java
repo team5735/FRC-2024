30e9f8c26b1c2481088982bdd89fbb36fc3897b2
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2024-03-25 20:28:32 -0400

    delete io style visions and rewrite llsubsystem

---
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -0,0 +1,46 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.networktables.DoubleArraySubscriber;
+import edu.wpi.first.networktables.DoublePublisher;
+import edu.wpi.first.networktables.DoubleSubscriber;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LimelightSubsystem extends SubsystemBase {
+    private static NetworkTable limelightTable = NetworkTableInstance.getDefault().getTable("limelight");
+
+    private DoubleArraySubscriber botposeSubscriber = limelightTable.getDoubleArrayTopic("botpose")
+            .subscribe(new double[8]);
+    private DoubleSubscriber targetVisibleSubscriber = limelightTable.getDoubleTopic("tv").subscribe(0);
+    private DoublePublisher ledModePublisher = limelightTable.getDoubleTopic("ledMode").publish();
+
+    public Pose3d getBotPose() {
+        double[] botpose = botposeSubscriber.get();
+        return new Pose3d(
+                botpose[0],
+                botpose[1],
+                botpose[2],
+                new Rotation3d(botpose[3],
+                        botpose[4],
+                        botpose[5]));
+    }
+
+    public boolean hasTarget() {
+        return targetVisibleSubscriber.get() == 1;
+    }
+
+    public int numTags() {
+        return ((int) botposeSubscriber.get()[7]);
+    }
+
+    public void ledsOn() {
+        ledModePublisher.set(3);
+    }
+
+    public void ledsOff() {
+        ledModePublisher.set(1);
+    }
+}
--- src/main/java/frc/robot/subsystems/vision/VisionResults.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.subsystems.vision;
-
-import edu.wpi.first.math.geometry.Pose3d;
-
-public class VisionResults {
-    public Pose3d robotPose = new Pose3d();
-    public double timeStamp = 0.0;
-}
