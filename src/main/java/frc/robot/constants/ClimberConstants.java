0551017a1f2b7082ef2e51c6b42e57ec6bb73d1a
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2024-05-16 12:16:22 -0400

    document ClimberConstants.java

--- src/main/java/frc/robot/constants/ClimberConstants.java
+++ src/main/java/frc/robot/constants/ClimberConstants.java
@@ -1,6 +1,9 @@
 package frc.robot.constants;
 
 public class ClimberConstants {
+    /**
+     * Voltages for pushing the climber motor up or down.
+     */
     public static final double UP_VOLTS = 8.0;
     public static final double DOWN_VOLTS = 8.0;
 }
