f50d9558bc08bf1e882f50e8f23b6de044043a15
Jacoby Adwin <jacoby_adwin@student.waylandps.org>
2024-05-16 12:38:00 -0400

    Finish JavaDocs on IntakeConstants

--- src/main/java/frc/robot/constants/FeederConstants.java
+++ src/main/java/frc/robot/constants/FeederConstants.java
@@ -7,8 +7,11 @@ package frc.robot.constants;
  */
 public class FeederConstants {
     /**
-     * Voltages to feed to the feeder motor.
+     * Voltage to feed to the intake motor when pulling notes in (to the shooter).
      */
     public static final double PULL_VOLTS = 2.5;
+    /**
+     * Voltage to feed to the intake motor when pushing notes out (to the intake).
+     */
     public static final double PUSH_VOLTS = 2.5;
 }
