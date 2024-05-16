fe10947bc2e0d23e7f91998121ed1d78cd6b18b7
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2024-05-16 12:31:06 -0400

    document FeederConstants.java

--- src/main/java/frc/robot/constants/FeederConstants.java
+++ src/main/java/frc/robot/constants/FeederConstants.java
@@ -1,6 +1,9 @@
 package frc.robot.constants;
 
 public class FeederConstants {
+    /**
+     * Voltages to feed to the feeder motor.
+     */
     public static final double PULL_VOLTS = 2.5;
     public static final double PUSH_VOLTS = 2.5;
 }
