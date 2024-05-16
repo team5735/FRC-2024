14a87f29d78f46a59c4df6b5a52e5fbc8c434801
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2024-05-16 12:31:13 -0400

    document IntakeConstants.java

--- src/main/java/frc/robot/constants/IntakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -1,6 +1,9 @@
 package frc.robot.constants;
 
 public class IntakeConstants {
+    /**
+     * Voltages to feed to the intake motor.
+     */
     public static final double PULL_VOLTS = 10;
     public static final double PUSH_VOLTS = 6;
 }
