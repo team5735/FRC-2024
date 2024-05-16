d0bbe34a764ef9b414357d5ae4ad3fb0bd5d8432
Jacoby Adwin <jacoby_adwin@student.waylandps.org>
2024-05-16 12:28:48 -0400

    Docs for FeederConstants and IntakeConstants

--- src/main/java/frc/robot/constants/FeederConstants.java
+++ src/main/java/frc/robot/constants/FeederConstants.java
@@ -1,6 +1,13 @@
 package frc.robot.constants;
 
+/**
+ * Constants file for storing some public static final data, hence constants.
+ * This file is specifically intended to be used in conjunction
+ * with {@link frc.robot.subsystems.FeederSubsystem}.
+ */
 public class FeederConstants {
+    // Voltage to be applied when pulling note (to shooter) and pushing out,
+    // respectively
     public static final double PULL_VOLTS = 2.5;
     public static final double PUSH_VOLTS = 2.5;
 }
--- src/main/java/frc/robot/constants/IntakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -1,6 +1,14 @@
 package frc.robot.constants;
 
+/**
+ * Constants file for storing some public static final data, hence constants.
+ * This file is specifically intended to be used in conjunction
+ * with {@link frc.robot.subsystems.IntakeSubsystem}.
+ */
+
 public class IntakeConstants {
+    // Voltage to be applied when pulling note (to feeder) and pushing out,
+    // respectively
     public static final double PULL_VOLTS = 10;
     public static final double PUSH_VOLTS = 6;
 }
