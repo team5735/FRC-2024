f1a0929c15c8cf2a9c623f702c5d4443d452e57b
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2024-05-16 10:44:55 -0400

    document CANdleConstants.java

--- src/main/java/frc/robot/constants/CANdleConstants.java
+++ src/main/java/frc/robot/constants/CANdleConstants.java
@@ -3,6 +3,10 @@ package frc.robot.constants;
 import java.awt.Color;
 
 public class CANdleConstants {
+    /**
+     * Indices into the COLORS array. To access a specific color, use e.g.
+     * COLORS[READY]. This is used for easier currying.
+     */
     public static final int READY = 0;
     public static final int AUTO = 1;
     public static final int AIMING = 2;
@@ -10,12 +14,12 @@ public class CANdleConstants {
     public static final int INTAKE_RUNNING = 4;
     public static final int SHOOTING = 5;
 
-    public static final Color[] COLORS = { 
-        new Color(0, 0, 255), // BLUE (READY)
-        new Color(127, 0, 127), // PURPLE (AUTO)
-        new Color(127, 127, 0), // YELLOW-GREEN (AIMING)
-        new Color(0, 255, 0),  // GREEN (AIMED)
-        new Color(127, 0, 0), // DIM RED (INTAKING)
-        new Color(255, 0, 0)  // RED (SHOOTING)
+    public static final Color[] COLORS = {
+            new Color(0, 0, 255), // BLUE (READY)
+            new Color(127, 0, 127), // PURPLE (AUTO)
+            new Color(127, 127, 0), // YELLOW-GREEN (AIMING)
+            new Color(0, 255, 0), // GREEN (AIMED)
+            new Color(127, 0, 0), // DIM RED (INTAKING)
+            new Color(255, 0, 0) // RED (SHOOTING)
     };
 }
