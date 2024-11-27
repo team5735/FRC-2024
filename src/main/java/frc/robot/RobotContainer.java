9468e1db2ffc0995e93bebb564ad8cbebd62a292
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2024-11-27 10:14:04 -0500

    japanese

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -97,11 +97,11 @@ public class RobotContainer {
         // configureSubsystemBindings();
     }
 
-    private static double deadband(double input) {
-        if (Math.abs(input) <= DrivetrainConstants.DEADBAND) {
+    private static double deadband(double 入力) {
+        if (Math.abs(入力) <= DrivetrainConstants.DEADBAND) {
             return 0;
         }
-        return input;
+        return 入力;
     }
 
     private TunableNumber drivetrainTargetAngle = new TunableNumber("turn to radians", -0.785);
