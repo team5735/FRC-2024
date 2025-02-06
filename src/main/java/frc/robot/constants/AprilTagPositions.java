5e0d4263392d53beb54dbabc31868bc33df843d9
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2025-02-06 09:07:54 -0500

    add all 6 red-alliance apriltags

--- src/main/java/frc/robot/constants/AprilTagPositions.java
+++ src/main/java/frc/robot/constants/AprilTagPositions.java
@@ -7,10 +7,10 @@ import edu.wpi.first.math.geometry.Translation2d;
 public class AprilTagPositions {
     // Tags around the Reef, ids 6-11. Field-space, centered at field origin (unlike
     // rest of code .-.)
-    Pose2d tags[] = new Pose2d[] {
+    public static final Pose2d tags[] = new Pose2d[] {
             // 6
             new Pose2d(
-                    new Translation2d(4.700446, -0.719682),
+                    new Translation2d(4.700446, -0.719482),
                     new Rotation2d(-60)),
             // 7
             new Pose2d(
@@ -18,7 +18,19 @@ public class AprilTagPositions {
                     new Rotation2d(0)),
             // 8
             new Pose2d(
-                    new Translation2d(5.116498, 0.719682),
+                    new Translation2d(4.700446, 0.719482),
                     new Rotation2d(60)),
+            // 9
+            new Pose2d(
+                    new Translation2d(3.869358, 0.719482),
+                    new Rotation2d(120)),
+            // 10
+            new Pose2d(
+                    new Translation2d(3.453306, 0),
+                    new Rotation2d(180)),
+            // 11
+            new Pose2d(
+                    new Translation2d(3.869358, -0.719482),
+                    new Rotation2d(-120)),
     };
 }
